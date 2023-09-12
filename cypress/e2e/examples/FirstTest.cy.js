977b66dee814bc2ae13ca895014bcbd69db0e926
Ashish Prajapati <ashishprajapati012@gmail.com>
2023-09-12 16:56:40 +0530

    12th September work

--- cypress/e2e/examples/FirstTest.cy.js
+++ cypress/e2e/examples/FirstTest.cy.js
@@ -1,3 +1,5 @@
+//<reference type="cypress"/>
+
 describe('UserLove Test', () => {
 
   it('Verify Login functionality', () => {
@@ -10,21 +12,47 @@ describe('UserLove Test', () => {
     cy.wait(4000)
 
 
-    cy.xpath('//*[@id=":r0:"]').type('lopeduru@clout.wiki');
-    cy.xpath('//*[@id=":r1:"]').type('Test@123');
-    cy.get('.gs-primary-button').click();
+    cy.xpath('//*[@id=":r0:"]').should('be.visible').should('be.enabled').type('lopeduru@clout.wiki');
+    cy.xpath('//*[@id=":r1:"]').should('be.visible').should('be.enabled').type('Test@123');
+    cy.get('.gs-primary-button').should('be.visible').should('be.enabled').click();
     cy.wait(5000);
 
     cy.contains('Bounce rate').scrollIntoView({ duration: 4000 });   // scroll smoothly      
     cy.wait(3000);
-    cy.log('created tour')
-    cy.xpath('/html/body/div/div/div[2]/div[1]/div/div/div/a[2]').click();    
-    
-    cy.xpath('/html/body/div/div/div[2]/div[1]/div/div/div/a[3]').click();
-    cy.xpath('/html/body/div/div/div[2]/div[1]/div/div/div/a[4]').click();
-    cy.xpath('/html/body/div/div/div[2]/div[1]/div/div/div/a[5]').click();
-    cy.xpath('/html/body/div/div/div[2]/div[1]/div/div/div/a[6]').click();
-    cy.xpath('/html/body/div/div/div[2]/div[1]/div/div/div/a[7]').click();
+
+
+    // Setting
+    cy.xpath('/html/body/div/div/div[1]/div[2]/button[2]/div[2]').should('be.visible').click(); cy.wait(2000); 
+    cy.xpath('/html/body/div[2]/div[3]/div/div[5]/button').should('be.visible').click();cy.title().should('eq', 'Userlove | Profile'); cy.wait(2000);
+    cy.xpath('/html/body/div/div/div[2]/div[1]/div/div[2]/a[2]').should('be.visible').click(); cy.wait(2000);
+    cy.xpath('/html/body/div/div/div[2]/div[1]/div/div[2]/a[3]').should('be.visible').click(); cy.wait(2000);
+    cy.xpath('/html/body/div/div/div[2]/div[1]/div/div[2]/a[4]').should('be.visible').click(); cy.wait(2000);
+    cy.xpath('/html/body/div/div/div[2]/div[1]/div/div[2]/a[5]').should('be.visible').click(); cy.wait(2000);
+    cy.xpath('/html/body/div/div/div[2]/div[1]/div/div[2]/a[6]').should('be.visible').click(); cy.wait(2000);
+    cy.xpath('/html/body/div/div/div[2]/div[1]/div/div[2]/a[7]').should('be.visible').click(); cy.wait(2000);
+    cy.xpath('/html/body/div/div/div[2]/div[1]/div/div[2]/a[8]').should('be.visible').click(); cy.wait(2000);
+    cy.xpath('/html/body/div/div/div[2]/div[1]/div/div[2]/a[9]').should('be.visible').click(); cy.wait(2000);
+    cy.xpath('/html/body/div/div/div[2]/div[1]/div/div[2]/a[10]').should('be.visible').click(); cy.wait(2000);
+    //Tour
+    cy.xpath('/html/body/div/div/div[2]/div[1]/div/div/div/a[2]').should('be.visible').click();
+    cy.get('.ContentWrapper_gs-content-wrapper-container-children__Qh5L1').scrollIntoView({ duration: 4000 });
+    cy.wait(2000);
+
+    //Checklist
+    cy.xpath('/html/body/div/div/div[2]/div[1]/div/div/div/a[3]').should('be.visible').click();
+    cy.wait(2000);
+    //Survey
+    cy.xpath('/html/body/div/div/div[2]/div[1]/div/div/div/a[4]').should('be.visible').click();
+    cy.wait(2000);
+    //Users
+    cy.xpath('/html/body/div/div/div[2]/div[1]/div/div/div/a[5]').should('be.visible').click();
+    cy.wait(2000);
+    //Events
+    cy.xpath('/html/body/div/div/div[2]/div[1]/div/div/div/a[6]').should('be.visible').click();
+    cy.wait(2000);
+    //Theme
+    cy.xpath('/html/body/div/div/div[2]/div[1]/div/div/div/a[7]').should('be.visible').click();
+
   })
 
 
